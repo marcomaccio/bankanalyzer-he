051aa89a9aa7a85c35f9170ad2555d47d429b224
Marco Maccio <marco.maccio@marmac.name>
2015-10-23 13:01:41 +0200

    Added the methods equals, hasCode,toString & javadoc

--- ba-dal/ba-dal-impl-jpa/src/main/java/name/marmac/bankanalyzer/model/impl/jpa/Transaction.java
+++ ba-dal/ba-dal-impl-jpa/src/main/java/name/marmac/bankanalyzer/model/impl/jpa/Transaction.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Created by marcomaccio on 10/09/2015.
@@ -37,6 +38,10 @@ public class Transaction implements TransactionPO {
     private Date            createdDate;
     private Date            lastUpdate;
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Id
     @Column(name = "pkId", nullable = false)
@@ -45,6 +50,10 @@ public class Transaction implements TransactionPO {
         return id;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "executionDate", nullable = false)
     @Temporal(TemporalType.DATE)
@@ -52,6 +61,10 @@ public class Transaction implements TransactionPO {
         return executionDate;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "valueDate", nullable = false)
     @Temporal(TemporalType.DATE)
@@ -59,42 +72,71 @@ public class Transaction implements TransactionPO {
         return valueDate;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "description", nullable = false)
     public String getDescription() {
         return description;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "amount", nullable = false)
     public float getAmount() {
         return amount;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "currency", nullable = false)
     public String getCurrency() {
         return currency;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "balance", nullable = false)
     public float getBalance() {
         return balance;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "category", nullable = true)
     public String getCategory() {
         return category;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "subCategory", nullable = true)
     public String getSubCategory() {
         return subCategory;
     }
 
+
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "createdDate", nullable = true)
     @Temporal(TemporalType.TIMESTAMP)
@@ -102,6 +144,10 @@ public class Transaction implements TransactionPO {
         return createdDate;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Column(name = "lastUpdate", nullable = true)
     @Temporal(TemporalType.TIMESTAMP)
@@ -109,6 +155,10 @@ public class Transaction implements TransactionPO {
         return lastUpdate;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @Version
     @Column(name = "version", nullable = true)
@@ -116,6 +166,10 @@ public class Transaction implements TransactionPO {
         return version;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     @ManyToOne(targetEntity = BankAccount.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "BANKACC_ID", nullable = false)
@@ -125,67 +179,173 @@ public class Transaction implements TransactionPO {
 
     /** Setters Methods - START **/
 
+    /**
+     *
+     * @param id
+     */
     @Override
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @param executionDate
+     */
     @Override
     public void setExecutionDate(Date executionDate) {
         this.executionDate = executionDate;
     }
 
+    /**
+     *
+     * @param valueDate
+     */
     @Override
     public void setValueDate(Date valueDate) {
         this.valueDate = valueDate;
     }
 
+    /**
+     *
+     * @param description
+     */
     @Override
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     *
+     * @param amount
+     */
     @Override
     public void setAmount(float amount) {
         this.amount = amount;
     }
 
+    /**
+     *
+     * @param currency
+     */
     @Override
     public void setCurrency(String currency) {
         this.currency = currency;
     }
 
+    /**
+     *
+     * @param balance
+     */
     @Override
     public void setBalance(float balance) {
         this.balance = balance;
     }
 
+    /**
+     *
+     * @param category
+     */
     @Override
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     *
+     * @param subCategory
+     */
     @Override
     public void setSubCategory(String subCategory) {
         this.subCategory = category;
     }
 
+    /**
+     *
+     * @param bankAccount
+     */
     @Override
     public void setBankAccount(BankAccountPO bankAccount) {this.bankAccount = bankAccount;}
 
+    /**
+     *
+     * @param createdDate
+     */
     @Override
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
     }
 
+    /**
+     *
+     * @param lastUpdate
+     */
     @Override
     public void setLastUpdate(Date lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
 
+    /**
+     *
+     * @param version
+     */
     @Override
     public void setVersion(Long version) {
         this.version = version;
     }
 
+    /**
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Float.compare(that.amount, amount) == 0 &&
+                Float.compare(that.balance, balance) == 0 &&
+                Objects.equals(executionDate, that.executionDate) &&
+                Objects.equals(valueDate, that.valueDate) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(currency, that.currency) &&
+                Objects.equals(bankAccount, that.bankAccount);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(executionDate, valueDate, description, amount, balance, currency, bankAccount);
+    }
+
+    @Override
+    public String toString() {
+        LOGGER.debug("Transaction{" +
+                        "executionDate=" + executionDate +
+                        ", valueDate=" + valueDate +
+                        ", description='" + description + '\'' +
+                        ", amount=" + amount +
+                        ", balance=" + balance +
+                        ", currency='" + currency + '\'' +
+                        ", category='" + category + '\'' +
+                        ", subCategory='" + subCategory + '\'' +
+                        ", bankAccount=" + bankAccount +
+                        '}');
+
+        return "Transaction{" +
+                "executionDate=" + executionDate +
+                ", valueDate=" + valueDate +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                ", category='" + category + '\'' +
+                ", subCategory='" + subCategory + '\'' +
+                ", bankAccount=" + bankAccount +
+                '}';
+    }
 }
