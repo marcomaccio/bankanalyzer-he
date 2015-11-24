b38b2147dbb9becdbe4967c79b2234d863cc02b7
Marco Maccio <marco.maccio@marmac.name>
2015-11-24 19:34:32 +0100

    Added DbUnit to the Integration Test at REST Layer
    added the configuration to allow using DbUnit for Integration Tests at
    REST Layer

--- ba-services/ba-services-rest-impl-jaxrs/src/integration-test/java/name/marmac/bankanalyzer/services/rest/tests/it/ITRetrieveBankAccountsTest.java
+++ ba-services/ba-services-rest-impl-jaxrs/src/integration-test/java/name/marmac/bankanalyzer/services/rest/tests/it/ITRetrieveBankAccountsTest.java
@@ -1,5 +1,8 @@
 package name.marmac.bankanalyzer.services.rest.tests.it;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import name.marmac.bankanalyzer.services.rest.client.webclient.BankAnalyzerWebClient;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,7 +10,10 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +25,12 @@ import javax.ws.rs.core.Response;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/rest-client-integrationtest-context.xml"})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
 public class ITRetrieveBankAccountsTest {
 
     public static final transient Logger LOGGER = LoggerFactory.getLogger(ITRetrieveBankAccountsTest.class);
@@ -38,6 +50,7 @@ public class ITRetrieveBankAccountsTest {
      *
      */
     @Test
+    @DatabaseSetup("retrieve-bankaccounts-setup-01.xml")
     public void testGetBankAccounts() {
         LOGGER.info("TEST: getBankAccounts");
         Response response = bankAnalyzerWebClient.getBankAccounts(MediaType.APPLICATION_JSON_TYPE, MediaType.APPLICATION_JSON_TYPE);
